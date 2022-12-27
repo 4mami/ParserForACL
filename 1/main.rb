ddb9a4ab4cf5b7e34d8d17cd8ec33fa2e5ef5e76
4mami <76460459+4mami@users.noreply.github.com>
2022-12-27 14:30:13 +0900

    [add] 動作確認用メソッドを追加

--- 1/main.rb
+++ 1/main.rb
@@ -1,6 +1,46 @@
 require './lexer'
 require './parser'
 
+def testForOutput
+  puts "\n"
+  puts '------動作確認結果------'
+  outputs = Dir.glob('./output/*.out').sort
+  count = 0
+  outputs.each do |output_file|
+    filename = output_file.slice(/\d.+/)
+    is_next = false
+
+    # 出力したファイルが正常なコードに対する出力であるなら
+    if filename.slice(3, 4) == 'pass'
+      File.open(output_file).each do |line|
+        if line.include?('error!')
+          puts "NG: #{filename}"
+          count += 1
+          is_next = true
+          break
+        end
+      end
+      next if is_next
+      puts "OK: #{filename}"
+      count += 1
+    # 出力したファイルがエラーとなるコードに対する出力であるなら
+    else
+      File.open(output_file).each do |line|
+        if line.include?('error!')
+          puts "OK: #{filename}"
+          count += 1
+          is_next = true
+          break
+        end
+      end
+      next if is_next
+      puts "NG: #{filename}"
+      count += 1
+    end
+  end
+  puts "#{count}/#{outputs.size}ファイルの動作確認を完了"
+end
+
 def main
   # inputフォルダがあるなら
   if Dir.exist?('./input')
@@ -24,7 +64,8 @@ def main
         end
       end
 
-    return
+      testForOutput
+      return
     end
   end
   mylex = Lexer.new($stdin)
